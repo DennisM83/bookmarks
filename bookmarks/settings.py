3b428217ba3e0cb9f241592fa006cd7d43227ccb
Dennis Wenrich <dennisfinley@gmail.com>
2022-12-31 15:32:39 -0500

    update

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -107,7 +107,7 @@ DATABASES = {
 
 }
 
-CSRF_TRUSTED_ORIGINS = ['https://bookmarks-production-78da.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = ['https://bookmarks-production-78da.up.railway.app']
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
